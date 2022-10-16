1f3247eabbc1fe35d46e11cae2cf3dc38a86c3bf
Паша Луковников <lukovnikov.pasha@gmail.com>
2022-10-16 15:23:02 +1000

    ++

--- WebShellAsp2/wwwroot/js/site.js
+++ WebShellAsp2/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function toggledark() {
+    if (localStorage.getItem('theme') === 'dark') {
+        localStorage.removeItem('theme');
+        document.body.classList.remove("dark");
+    } else {
+        localStorage.setItem('theme', 'dark');
+        document.body.classList.add("dark");
+    }
+}
+if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark');
+    document.getElementById('toggledark').checked = true;
+} else {
+    document.body.classList.remove('dark');
+    document.getElementById('toggledark').checked = false;
+}
