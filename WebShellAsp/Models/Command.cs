62fd9d0d43af84838942d5b3408db726836e6927
Pasha-kto <lukovnikov.pasha@gmail.com>
2023-02-06 13:49:55 +1000

    ++

--- WebShellAsp/Models/Command.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebShellAsp.Models
-{
-    public class Command
-    {
-        public int ID { get; set; }
-        public string? CommandName { get; set; }
-        public string? CommandParametr { get; set; }
-    }
-}
